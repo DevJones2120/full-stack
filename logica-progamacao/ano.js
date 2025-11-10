669d2f0f35ecfbb3d54e0511e67cf69eb18f02b3
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-11-10 16:25:35 -0300

    Adicionando copa e ano de acontecimento

---
+++ logica-progamacao/ano.js
@@ -0,0 +1,7 @@
+// Criar um programa, para verificar o ano de nascimento
+const anoAtual = 2025;
+
+let nome = prompt("Insira seu nome")
+let idade = prompt("Inisra o ano que voce nasceu")
+
+document.write("Olá " , nome , ", você nasceu em " , idade , " e você tem " , anoAtual - idade , " anos !")
---
+++ logica-progamacao/copa.js
@@ -0,0 +1,5 @@
+const anoAtual = 2025
+
+//let anoCopa = prompt("A cada quantos anos ocorre a copa do mundo ?")
+
+document.write("A próxima Copa vai ocorrer em " , anoAtual + 1)
