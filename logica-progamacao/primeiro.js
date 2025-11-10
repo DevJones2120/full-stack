edca3ce5f1558d59df12d573573653c05fe6efe6
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-11-10 15:09:27 -0300

    Iniciando prática com lógica

---
+++ logica-progamacao/primeiro.js
@@ -0,0 +1,13 @@
+// Perguntando nome para usuário
+const userName = prompt("Qual é seu nome");
+
+// Perguntando qual é sua idade
+const userAge = prompt("E qual sua idade")
+
+// Imprimindo na tela o resultado
+document.write("Seja bem vindo, sr." + "<strong>" + userName + "</strong>" + "😊");
+
+// Imprimindo na tela a idade
+document.write("<br>" + "Nossa !" + "<br>" + "você tem " + userAge + " anos !" )
+
+
---
+++ logica-progamacao/segundo.js
@@ -0,0 +1,4 @@
+let numero1 = parseInt(prompt("Digite um número"));
+let numero2 = parseInt(prompt("Digite outro número"));
+
+console.log(numero1 + numero2)
