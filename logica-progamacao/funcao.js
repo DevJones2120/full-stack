2bd087dc7c92e447acc206760d4e31b9580cab63
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-11-11 16:19:41 -0300

    Trabalhando com funções

---
+++ logica-progamacao/funcao.js
@@ -0,0 +1,20 @@
+function resultAnimals() {
+    console.log("Animais");
+}
+
+function timeFutebol(time) {
+    document.write(time , "<br>");
+}
+
+// Criando função para pular linha
+function pularlinha() {
+    document.write("<br>")
+}
+
+// Para executar a função
+resultAnimals();
+
+timeFutebol("Real Madrid");
+pularlinha();//Usando a função de pular linha
+timeFutebol("Corinthians");
+timeFutebol("Arsenal");
---
+++ logica-progamacao/programa1.js
@@ -0,0 +1,16 @@
+/*
+    Vamos criar um programa, que o usuário executa uma função e dentro função existe uma frase.
+    Polde pular linhas.
+*/ 
+
+function conviteFesta(convite) {
+    document.write(convite)
+}
+
+function pularlinha() {
+    document.write("<br>")
+}
+
+conviteFesta("Seja bem vindo Jones");
+pularlinha();
+conviteFesta("Seja bem vindo zeze");
