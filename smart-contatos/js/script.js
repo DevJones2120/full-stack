47a6b5dc19b25cca70a156e957da68913e117446
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-11-18 16:18:34 -0300

    Criando lista com dados e limpando formulário

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,5 +1,6 @@
 
 const form = document.querySelector("form");
+const lista = document.querySelector(".lista");
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTel = document.getElementById("telefone");
@@ -7,26 +8,40 @@ const inputTel = document.getElementById("telefone");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    // Validação do formulário
+    // VALIDAÇÃO DO FORMULÁRIO
     // se o imput for vazio, adicionar mensagem
-
-    if (inputNome.value == "") {
+    if (inputNome.value == "" || inputEmail.value == "" || inputTel.value == "") {
         alert("Insira seus dados por favor.")
         return false;
+
+    // MENSAGENS IRÃO APARECER EM CADA FORMULÁRIO
+    // if (inputNome.value == "") {
+    //     alert("Insira seu nome por favor.")
+    //     return false;
+    // }--
+
+    // if (inputEmail.value == "") {
+    //     alert("Insira seu email por favor.")
+    //     return false;
+    // }
+
+    // if (inputTel.value == "") {
+    //     alert("Insira seu telefone por favor.")
+    //     return false;
+    // }
     }
 
-    if (inputEmail.value == "") {
-        alert("Insira seus dados por favor.")
-        return false;
-    }
 
-    if (inputTel.value == "") {
-        alert("Insira seus dados por favor.")
-        return false;
-    }
+    //CRIAR <li>
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span class="contato-nome">👤: ${inputNome.value}</span>
+            <span class="contato-email">📧: ${inputEmail.value}</span>
+            <span class="contato-tel">📞: ${inputTel.value}</span>
+        `;
+        lista.appendChild(li)
 
-    console.log(inputNome.value);
-    console.log(inputEmail.value);
-    console.log(inputTel.value);
+        //LIMPAR INPUTS
+        form.reset();
     
 })
