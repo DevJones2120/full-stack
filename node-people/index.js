3fbc43f8ad9cc8baac110fb4ad011e3fcfda6213
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-11-27 16:28:55 -0300

    Trabalhando rotas com array de times

--- node-people/index.js
+++ node-people/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 3000; //executar na porta 3000
 
+// Indicar para express ler o body com jason
+app.use(express.json());
+
 // mock
 const nomes = [
   { id: 1, nome: "Fernanda", idade: "18" },
@@ -10,6 +13,47 @@ const nomes = [
   { id: 4, nome: "Samuel", idade: "45" },
   { id: 5, nome: "Doris", idade: "33" },
 ];
+
+
+const times = [
+  { id: 1, nome: "Corinthians", estado: "SP", titulos: 7 },
+  { id: 2, nome: "Palmeiras", estado: "SP", titulos: 11 },
+  { id: 3, nome: "Santos", estado: "SP", titulos: 8 },
+  { id: 4, nome: "Flamengo", estado: "RJ", titulos: 7 },
+  { id: 5, nome: "Vasco", estado: "RJ", titulos: 4 },
+  { id: 6, nome: "Atlético Mineiro", estado: "MG", titulos: 3 },
+  { id: 7, nome: "Cruzeiro", estado: "MG", titulos: 4 },
+];
+
+//Função para buscar times
+app.get("/times", (req, res) => {
+    res.send(times)
+})
+
+// Função para buscar por id
+// Função auxiliar para buscar por id
+function buscarTimesPorId(id) {
+    return times.filter((timesFutebol) => timesFutebol.id == id)
+}
+
+app.get("/times/:id", (req,res) => {
+
+    let index = req.params.id;
+    res.json(buscarTimesPorId(index))
+})
+
+
+// Função para excluir 
+app.delete("/times/:id", (req, res) => {
+    let index = buscarTimesPorId(req.params.id)
+    times.splice(index, 1)
+    res.send(`Time com o id ${req.params.id} excluido com sucesso !`)
+});
+
+
+
+
+
 
 
 
@@ -67,7 +111,9 @@ app.delete("/ListaNomes/:id", (req, res) => {
     res.send(`Nomes com id ${req.params.id} excluida com sucesso !`)
 });
 
+
+
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
 });
-
