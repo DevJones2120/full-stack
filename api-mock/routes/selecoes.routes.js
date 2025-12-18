82c8b0fd18f89688608ace51196b2db366a75887
DevJones2120 <davi.jones.cruz21@gmail.com>
2025-12-18 15:53:56 -0300

    Adicionando todas as rotas do Old para routes e excluindo Old

--- api-mock/routes/selecoes.routes.js
+++ api-mock/routes/selecoes.routes.js
@@ -12,4 +12,46 @@ router.get("/selecoes", (req, res) => {
     })
 });
 
+// Buscando por ID
+router.get("/selecoes/:id", (req, res) => {
+    const id = req.params.id;
+    const sql = "select * from selecoes where id=?;" 
+
+    conexao.query(sql, id, (erro, result) => {
+        res.json(result[0])
+    });
+});
+
+
+// Criando POST para cadastrar
+router.post("/selecoes", (req, res) => {
+    const selecao = req.body;
+    const sql = "INSERT INTO selecoes SET?;";
+
+    conexao.query(sql, selecao, () => {
+        res.json({ mensagem: "Cadastrado com sucesso !"});
+    })
+});
+
+// Deletando registro
+router.delete("/selecoes/:id", (req, res) => {
+    const id = req.params.id;   
+    const sql = "delete from selecoes where id=?;";
+
+    conexao.query(sql, id, (erro, result) => {
+        res.json({mensagem: "Deletado com sucesso"});
+    });
+
+})
+
+// Atualizando registro
+router.put("/selecoes/:id", (req, res) => {
+    const id = req.params.id;
+    const selecao = req.body;
+    const sql = "update selecoes set ? where id=?";
+
+    conexao.query(sql, [selecao, id], ()=> {
+        res.json({ mensagem: "Atualizado com sucesso ✅!"});
+    });
+})
 export default router;
